e40b8c6f36dd914580b6d4e963b3d211242d2a5d
Yaroslav Shirokov <slava@github.com>
2012-11-24 22:39:17 -0800

    That was a stupid -Wno- anyway

--- make/commit.mk
+++ make/commit.mk
@@ -1,6 +1,6 @@
 OBJS = $(patsubst %.c,%.o,$(SOURCES))
 CPPFLAGS = $(OPTCPPFLAGS)
-CFLAGS = -g -O2 $(INCLUDE) $(OPTFLAGS) -Wall -Wno-incompatible-pointer-types
+CFLAGS = -g -O2 $(INCLUDE) $(OPTFLAGS) -Wall
 LIBS = $(OPTLIBS) -lm -lpthread -lcurl $(LUA_LIB)
 
 $(OBJS): $(SOURCES)
