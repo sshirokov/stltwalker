eb455d87e204a5d8d7102c5ba4dd2976fa2aab53
Yaroslav Shirokov <slava@github.com>
2012-11-25 16:43:50 -0800

    Warnings are now errors. Welcome to hard mode.

--- make/commit.mk
+++ make/commit.mk
@@ -1,6 +1,6 @@
 OBJS = $(patsubst %.c,%.o,$(SOURCES))
 CPPFLAGS = $(OPTCPPFLAGS)
-CFLAGS = -g -O2 $(INCLUDE) $(OPTFLAGS) -Wall
+CFLAGS = -g -O2 $(INCLUDE) $(OPTFLAGS) -Wall -Werror
 LIBS = $(OPTLIBS) -lm
 
 $(OBJS): $(SOURCES)
