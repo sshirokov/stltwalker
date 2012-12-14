ea945f38c9ad07c92d031f1693830ec4c0dbc9aa
Yaroslav Shirokov <slava@github.com>
2012-12-13 21:27:02 -0800

    Zero X #didn't test

--- src/transform.c
+++ src/transform.c
@@ -59,6 +59,15 @@ void object_transform_chain_zero_z(stl_transformer *t) {
 		transform_chain(t, *init_transform_translate_f(&tr, v));
 }
 
+void object_transform_chain_zero_x(stl_transformer *t) {
+		float3 bounds[2] = {{INF, INF, INF}, {-INF, -INF, -INF}};
+		float3 v = FLOAT3_INIT;
+		float4x4 tr;
+		object_bounds(t->object, &bounds[0], &bounds[1]);
+		f3X(v) = -f3X(bounds[0]);
+		transform_chain(t, *init_transform_translate_f(&tr, v));
+}
+
 // Conversion helpers
 float deg2rad(float deg) {
 		return deg * (PI / 180.0);
