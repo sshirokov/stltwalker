8c133bfd41ce645b63d86946b7b5bff50fe17a42
Yaroslav Shirokov <slava@github.com>
2012-12-13 21:37:17 -0800

    WIP: Maybe even correctly center on X/Y

--- src/transform.c
+++ src/transform.c
@@ -64,7 +64,7 @@ void object_transform_chain_zero_x(stl_transformer *t) {
 		float3 v = FLOAT3_INIT;
 		float4x4 tr;
 		object_bounds(t->object, &bounds[0], &bounds[1]);
-		f3X(v) = -f3X(bounds[0]);
+		f3X(v) = -(f3X(bounds[1]) - f3X(bounds[0]));
 		transform_chain(t, *init_transform_translate_f(&tr, v));
 }
 
@@ -73,7 +73,7 @@ void object_transform_chain_zero_y(stl_transformer *t) {
 		float3 v = FLOAT3_INIT;
 		float4x4 tr;
 		object_bounds(t->object, &bounds[0], &bounds[1]);
-		f3Y(v) = -f3Y(bounds[0]);
+		f3Y(v) = -(f3Y(bounds[1]) - f3Y(bounds[0]));
 		transform_chain(t, *init_transform_translate_f(&tr, v));
 }
 
